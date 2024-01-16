ad08d4df3653f659d4e43d6b389b26af87317fb4
AebyZayn <115506566+aebyzayn@users.noreply.github.com>
2024-01-16 20:42:57 +0600

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,123 @@
+// Header Data..................
+
+import HeroImage from '../assets/hero.png'
+
+export const headerData =[
+    {
+        id: 1,
+        heroImage: HeroImage,
+        intro: 'Hello! I’m', 
+        name: 'AebyZayn',
+        profession: 'A Developer who',
+        hedingText: 'Build modern idea for modern',    
+        hedingSp: 'programs',
+        pText: 'Because if the idea does not impress you what else can?'   
+    }
+]
+
+
+// About Data..................
+
+import ytIcon from '../assets/yt.png'
+
+export const aboutData =[
+    {
+        id: 1,
+        yt: ytIcon,
+        infoText: "Currently, I'm a Software Engineer at",
+        link: '#',
+        linkText: 'Youtube',
+        aboutPara: 'A self-taught UI/UX designer, functioning in the industry for 3+ years now. I make meaningful and delightful digital products that create an equilibrium between user needs and business goals.'
+    }
+]
+
+
+
+//  Experience Data..................
+
+import cardIcon1 from '../assets/cardIcon1.png'
+import cardIcon2 from '../assets/cardIcon2.png'
+import cardIcon3 from '../assets/cardIcon3.png'
+import cardIcon4 from '../assets/cardIcon4.png'
+
+export const experienceData =[
+    {
+        id: 1,
+        icon: cardIcon1,
+        cardName: 'CIB on the Mobile',
+        cardInfo: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+        cardLink: '#'
+    },
+    {
+        id: 2,
+        icon: cardIcon2,
+        cardName: 'CIB on the Mobile',
+        cardInfo: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+        cardLink: '#'
+    },
+    {
+        id: 3,
+        icon: cardIcon3,
+        cardName: 'CIB on the Mobile',
+        cardInfo: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+        cardLink: '#'
+    },
+    {
+        id: 4,
+        icon: cardIcon4,
+        cardName: 'CIB on the Mobile',
+        cardInfo: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+        cardLink: '#'
+    },
+]
+
+
+
+
+//  Projects Data..................
+
+import project1 from '../assets/project1.png'
+import project2 from '../assets/project2.png'
+
+
+export const projectsData =[
+    {
+        id: 1,
+        img: project1,
+        name: 'Frontend Project 1',
+        des: 'A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.',
+        projectLink: '#'
+    },
+    {
+        id: 2,
+        img: project2,
+        name: 'Frontend Project 2',
+        des: 'A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.',
+        projectLink: '#'
+    },
+   
+]
+
+
+//  Projects Data..................
+
+export const testimonialssData =[
+    {
+        id: 1,
+        text: 'Exceptionally pleased with militaria. I have been very disappointed with many alleged business card designers. He really surprised me with his taste and style. Impossible to let you down.',
+        clintName: 'Ashlynn Herwit',
+        profession: 'Interior Designer'
+    },
+    {
+        id: 1,
+        text: 'Exceptionally pleased with militaria. I have been very disappointed with many alleged business card designers. He really surprised me with his taste and style. Impossible to let you down.',
+        clintName: 'Habib Wahid',
+        profession: 'Video Editor'
+    },
+    {
+        id: 1,
+        text: 'Exceptionally pleased with militaria. I have been very disappointed with many alleged business card designers. He really surprised me with his taste and style. Impossible to let you down.',
+        clintName: 'Mohammed Abir',
+        profession: 'Web Developer'
+    },
+]
